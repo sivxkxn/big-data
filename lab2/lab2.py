3d1a84d1d49319c0dd00146da700e7a8cd7f5706
sivxkxn <sivxkxn@gmail.com>
2022-03-26 00:17:34 +0200

    lab2 is completed

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,9 +1,16 @@
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt 
+import seaborn as sns 
+import matplotlib.pyplot as plt
 brainFile = 'brainsize.txt' 
 brainFrame = pd.read_csv(brainFile)
-brainFrame.corr(method='pearson')
+menDf = brainFrame[(brainFrame.Gender == 'Male')] 
+# womenDf = brainFrame[(brainFrame.Gender == 'Female')]
+mcorr = menDf.corr() 
+sns.heatmap(mcorr)
+plt.savefig('attribute_correlations2.png', tight_layout=True) 
+# print(womenDf.corr(method='pearson'))
 # menDf = brainFrame[(brainFrame.Gender == 'Male')] 
 # womenDf = brainFrame[(brainFrame.Gender == 'Female')]
 # womenMeanSmarts = womenDf[["PIQ", "FSIQ", "VIQ"]].mean(axis=1) 
